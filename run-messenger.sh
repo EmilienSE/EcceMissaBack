2d77d39c5b1ed72e3b8ef934bf5fac7fb414acd3
Emilien <esechilariu@yahoo.fr>
2025-02-07 12:13:05 +0100

    dev(recette): add message

--- run-messenger.sh
+++ run-messenger.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-php /home/eccemiz/api/bin/console messenger:consume async --limit=10 --no-debug
+for i in {1..60}  # Exécuter 60 fois pour chaque minute de l'heure
+do
+    /usr/local/php8.2/bin/php bin/console messenger:consume async --limit=10 --no-debug
+    sleep 60  # Attendre 60 secondes avant de recommencer
+done
